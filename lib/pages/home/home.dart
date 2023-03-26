9eac8052e9105a8c32a6027d0f43916965689123
Oleksandr Shmatko <oleksandr.shmatko@globallogic.com>
2023-03-26 02:09:14 +0200

    Home screen update.
    Signed-off-by: Oleksandr Shmatko <oleksandr.shmatko@globallogic.com>

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,6 +1,5 @@
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:aws_iot_api/iot-2015-05-28.dart';
-import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -10,12 +9,6 @@ import 'package:osh_remote/block/sign_in/sign_in_bloc.dart';
 import 'package:osh_remote/injection_container.dart';
 import 'package:osh_remote/pages/home/parts/home_body.dart';
 import 'package:osh_remote/pages/login/login_page.dart';
-
-const _kPages = <String, IconData>{
-  'monitoring': Icons.ssid_chart_rounded,
-  'home': Icons.home_rounded,
-  'settings': Icons.settings,
-};
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -34,20 +27,22 @@ class HomePage extends StatelessWidget {
     final mqttRepository =
         MqttServerClientRepository(region, credentials, server);
 
-    return RepositoryProvider.value(
-        value: mqttRepository,
-        child: MultiBlocProvider(
+    return
+      // RepositoryProvider.value(
+      //   value: mqttRepository,
+      //   child:
+    MultiBlocProvider(
           providers: [
             BlocProvider<SignInBloc>(
               create: (_) => SignInBloc(getIt<AuthenticationRepository>()),
             ),
             BlocProvider<MqttClientBloc>(
-              create: (BuildContext context) =>
-                  MqttClientBloc(repository: mqttRepository),
+              create: (_) => MqttClientBloc(repository: mqttRepository),
             ),
           ],
           child: const HomePageContent(),
-        ));
+        // )
+    );
   }
 }
 
@@ -61,8 +56,6 @@ class HomePageContent extends StatefulWidget {
 }
 
 class _HomePageContentState extends State<HomePageContent> {
-  int _selectedPage = 0;
-
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
@@ -91,66 +84,48 @@ class _HomePageContentState extends State<HomePageContent> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: DefaultTabController(
-            length: _kPages.length,
-            initialIndex: 1,
-            child: Scaffold(
-                appBar: AppBar(
-                  // actions: _isWaiting ? <Widget>[
-                  //   const CircularProgressIndicator()
-                  // ] : null,
-                  centerTitle: true,
-                  title: const Text("OSH remote"),
+        child: Scaffold(
+            body: const HomeBody(),
+            drawer: Drawer(
+                child: Column(
+              children: <Widget>[
+                _buildDrawerHeader(),
+                Builder(
+                  builder: (context) {
+                    final userId = context
+                        .select((SignInBloc bloc) => bloc.state.email.value);
+                    return Text('UserID: $userId');
+                  },
                 ),
-                body: _selectedPage == 1 ? const HomeBody() : null,
-                bottomNavigationBar:
-                    ConvexAppBar.badge(const <int, dynamic>{3: '99+'},
-                        style: TabStyle.textIn,
-                        items: <TabItem>[
-                          for (final entry in _kPages.entries)
-                            TabItem(icon: entry.value, title: entry.key),
-                        ],
-                        onTap: (int i) => setState(() => _selectedPage = i)),
-                drawer: Drawer(
-                    child: Column(
-                  children: <Widget>[
-                    _buildDrawerHeader(),
-                    Builder(
-                      builder: (context) {
-                        final userId = context.select(
-                            (SignInBloc bloc) => bloc.state.email.value);
-                        return Text('UserID: $userId');
-                      },
-                    ),
-                    ListTile(
-                      leading: const Icon(Icons.home),
-                      title: const Text('Device 1'),
-                      onTap: () => Navigator.pop(context),
-                    ),
-                    ListTile(
-                      leading: const Icon(Icons.home),
-                      title: const Text('Device 2'),
-                      onTap: () => Navigator.pop(context),
-                    ),
-                    ListTile(
-                      leading: const Icon(Icons.add),
-                      title: Text(S.of(context)!.addDevice),
-                      onTap: () => Navigator.pop(context),
-                    ),
-                    ListTile(
-                      leading: const Icon(Icons.settings),
-                      title: Text(S.of(context)!.settings),
-                      onTap: () => Navigator.pop(context),
-                    ),
-                    const Spacer(),
-                    const Divider(thickness: 1.5),
-                    ListTile(
-                      leading: const Icon(Icons.power_settings_new,
-                          color: Colors.red),
-                      title: Text(S.of(context)!.signOut),
-                      onTap: signOutCurrentUser,
-                    ),
-                  ],
-                )))));
+                ListTile(
+                  leading: const Icon(Icons.home),
+                  title: const Text('Device 1'),
+                  onTap: () => Navigator.pop(context),
+                ),
+                ListTile(
+                  leading: const Icon(Icons.home),
+                  title: const Text('Device 2'),
+                  onTap: () => Navigator.pop(context),
+                ),
+                ListTile(
+                  leading: const Icon(Icons.add),
+                  title: Text(S.of(context)!.addDevice),
+                  onTap: () => Navigator.pop(context),
+                ),
+                ListTile(
+                  leading: const Icon(Icons.settings),
+                  title: Text(S.of(context)!.settings),
+                  onTap: () => Navigator.pop(context),
+                ),
+                const Spacer(),
+                const Divider(thickness: 1.5),
+                ListTile(
+                  leading:
+                      const Icon(Icons.power_settings_new, color: Colors.red),
+                  title: Text(S.of(context)!.signOut),
+                  onTap: signOutCurrentUser,
+                ),
+              ],
+            ))));
   }
 }
--- lib/pages/home/parts/home_body.dart
+++ lib/pages/home/parts/home_body.dart
@@ -12,94 +12,118 @@ class HomeBody extends StatefulWidget {
 }
 
 class _HomeBodyState extends State<HomeBody> {
+  late ScrollController _scrollController;
+  static const kExpandedHeight = 300.0;
+  Widget? _title;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _scrollController = ScrollController()..addListener(() {
+      setState(() {
+        _title = _isSliverAppBarExpanded ? const Text("Title") : null;
+      });
+    });
+  }
+
+  bool get _isSliverAppBarExpanded {
+    return _scrollController.hasClients &&
+        _scrollController.offset > kExpandedHeight - kToolbarHeight;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: Colors.black.withOpacity(0.03),
-        child: CustomScrollView(
-          slivers: <Widget>[
-            const SliverAppBar(
-              pinned: true,
-              expandedHeight: 250.0,
-              flexibleSpace: FlexibleSpaceBar(
-                // title: Text('25.2°C'),
-                background: HomeTempIndicator(),
-              ),
-            ),
-            SliverList(
-              delegate: SliverChildListDelegate(
-                [
-                  const SelectorModeWidget(),
-                ],
-              ),
-            ),
-            SliverGrid(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisExtent: 120.0, //height
-              ),
-              delegate: SliverChildListDelegate(
-                [
-                  SmallHomeWidget(
-                      label: S.of(context)!.heater_status,
-                      value: "1/6",
-                      iconData: Icons.local_fire_department),
-                  SmallHomeWidget(
-                      label: S.of(context)!.pump_status,
-                      value: "46%",
-                      iconData: Icons.loop),
-                  SmallHomeWidget(
-                      label: S.of(context)!.temp_in,
-                      value: "31.0°C",
-                      iconData: Icons.arrow_upward),
-                  SmallHomeWidget(
-                      label: S.of(context)!.temp_out,
-                      value: "37.5°C",
-                      iconData: Icons.arrow_downward),
-                  SmallHomeWidget(
-                      label: S.of(context)!.pressure,
-                      value: "1.9bar",
-                      iconData: Icons.compare_arrows),
-                  SmallHomeWidget(
-                      label: S.of(context)!.power_usage,
-                      value: "32%",
-                      iconData: Icons.timelapse),
-                  SmallHomeWidget(
-                      label: S.of(context)!.pressure,
-                      value: "1.9bar",
-                      iconData: Icons.compare_arrows),
-                  SmallHomeWidget(
-                      label: S.of(context)!.power_usage,
-                      value: "32%",
-                      iconData: Icons.timelapse),
-                  SmallHomeWidget(
-                      label: S.of(context)!.pressure,
-                      value: "1.9bar",
-                      iconData: Icons.compare_arrows),
-                  SmallHomeWidget(
-                      label: S.of(context)!.power_usage,
-                      value: "32%",
-                      iconData: Icons.timelapse),
-                  SmallHomeWidget(
-                      label: S.of(context)!.pressure,
-                      value: "1.9bar",
-                      iconData: Icons.compare_arrows),
-                  SmallHomeWidget(
-                      label: S.of(context)!.power_usage,
-                      value: "32%",
-                      iconData: Icons.timelapse),
-                  SmallHomeWidget(
-                      label: S.of(context)!.pressure,
-                      value: "1.9bar",
-                      iconData: Icons.compare_arrows),
-                  SmallHomeWidget(
-                      label: S.of(context)!.power_usage,
-                      value: "32%",
-                      iconData: Icons.timelapse),
-                ],
-              ),
-            ),
+    return CustomScrollView(
+      controller: _scrollController,
+      slivers: <Widget>[
+        SliverAppBar(
+          title: _title,
+          centerTitle: true,
+          actions: [
+            IconButton(onPressed: () => {}, icon: const Icon(Icons.settings))
           ],
-        ));
+          pinned: true,
+          expandedHeight: kExpandedHeight,
+          flexibleSpace: const FlexibleSpaceBar(
+            background: HomeTempIndicator(),
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildListDelegate(
+            [
+              const SelectorModeWidget(),
+            ],
+          ),
+        ),
+        SliverGrid(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisExtent: 120.0, //height
+          ),
+          delegate: SliverChildListDelegate(
+            [
+              SmallHomeWidget(
+                  label: S.of(context)!.heater_status,
+                  value: "1/6",
+                  iconData: Icons.local_fire_department),
+              SmallHomeWidget(
+                  label: S.of(context)!.pump_status,
+                  value: "46%",
+                  iconData: Icons.loop),
+              SmallHomeWidget(
+                  label: S.of(context)!.temp_in,
+                  value: "31.0°C",
+                  iconData: Icons.arrow_upward),
+              SmallHomeWidget(
+                  label: S.of(context)!.temp_out,
+                  value: "37.5°C",
+                  iconData: Icons.arrow_downward),
+              SmallHomeWidget(
+                  label: S.of(context)!.pressure,
+                  value: "1.9bar",
+                  iconData: Icons.compare_arrows),
+              SmallHomeWidget(
+                  label: S.of(context)!.power_usage,
+                  value: "32%",
+                  iconData: Icons.timelapse),
+              SmallHomeWidget(
+                  label: S.of(context)!.pressure,
+                  value: "1.9bar",
+                  iconData: Icons.compare_arrows),
+              SmallHomeWidget(
+                  label: S.of(context)!.power_usage,
+                  value: "32%",
+                  iconData: Icons.timelapse),
+              SmallHomeWidget(
+                  label: S.of(context)!.pressure,
+                  value: "1.9bar",
+                  iconData: Icons.compare_arrows),
+              SmallHomeWidget(
+                  label: S.of(context)!.power_usage,
+                  value: "32%",
+                  iconData: Icons.timelapse),
+              SmallHomeWidget(
+                  label: S.of(context)!.pressure,
+                  value: "1.9bar",
+                  iconData: Icons.compare_arrows),
+              SmallHomeWidget(
+                  label: S.of(context)!.power_usage,
+                  value: "32%",
+                  iconData: Icons.timelapse),
+              SmallHomeWidget(
+                  label: S.of(context)!.pressure,
+                  value: "1.9bar",
+                  iconData: Icons.compare_arrows),
+              SmallHomeWidget(
+                  label: S.of(context)!.power_usage,
+                  value: "32%",
+                  iconData: Icons.timelapse),
+            ],
+          ),
+        ),
+      ],
+      // )
+    );
   }
 }
